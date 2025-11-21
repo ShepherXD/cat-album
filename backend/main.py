20e7d413b217522db3ae860c8f7eef0d32b05984
ShepherXD <yingzhengdev@gmail.com>
2025-11-22 00:03:33 +0900

    implement cat info addition

--- backend/main.py
+++ backend/main.py
@@ -1,21 +1,15 @@
-from typing import Union
+from typing import Optional, Union, IO
+import os
 from fastapi.middleware.cors import CORSMiddleware
-
-
-from fastapi import FastAPI, File, UploadFile
-
-app = FastAPI()
-
-
-@app.post("/files/")
-async def create_file(file: bytes = File()):
-    return {"file_size": len(file)}
-
-
-@app.post("/uploadfile/")
-async def create_upload_file(file: UploadFile):
-    return {"filename": file.filename}
+from fastapi import FastAPI, File, HTTPException, UploadFile
+from fastapi import Form
+from dotenv import load_dotenv
 from llm2server import cat_recognize
+import uuid
+import boto3
+from sqlmodel import SQLModel, Field, Session, create_engine
+
+load_dotenv()
 
 app = FastAPI()
 app.add_middleware(
@@ -26,6 +20,54 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+sqlite_url = "sqlite:///database.db"
+engine = create_engine(sqlite_url)
+
+@app.on_event("startup")
+def on_startup():
+    SQLModel.metadata.create_all(engine)
+
+s3 = boto3.client('s3',
+  endpoint_url = os.getenv("AWS_ENDPOINT"),
+)
+
+class Cat(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+    breed: str
+    remark:str
+    image_url: str
+
+def store_image(file: IO, filename:str) -> str:
+    ext = filename.split(".")[-1]
+    if ext.lower() not in ["jpg", "jpeg", "png", "gif"]:
+        raise HTTPException(status_code=400, detail="Invalid image format")
+    # UUID
+    filename = f"{str(uuid.uuid4())}.{ext}" 
+    s3.upload_fileobj(file, "cat-album", filename)
+    public_domain = os.getenv("AWS_PUBLIC_DOMAIN")
+    return f"{public_domain}/{filename}"
+
+
+@app.put("/cat", response_model=Cat)
+def add_cat(
+    name: str = Form(...),
+    breed: str = Form(...),
+    remark: str = Form(...),
+    image: UploadFile = File(...)
+) -> Cat:
+    assert image is not None, "Image file is required"
+    assert image.filename != "", "Image filename is required"
+    image_url = store_image(image.file, image.filename)
+    cat = Cat(name=name, breed=breed, remark=remark, image_url=image_url)
+    # store to database here
+    with Session(engine) as session:
+        session.add(cat)
+        session.commit()
+        session.refresh(cat)
+    
+    return cat
+     
 
 @app.get("/")
 def read_root():
