8aa84112971f7b649c24e0dbd4b9bf9063bf837b
ShepherXD <yingzhengdev@gmail.com>
2025-11-25 01:10:12 +0900

    optimize UI interaction（API‘s overloaded warning and retry）

--- frontend/src/components/addCat.vue
+++ frontend/src/components/addCat.vue
@@ -51,25 +51,41 @@
         @submit="(currCat)=>save(currCat)"
         @close="(currCat)=>save(currCat)"/>  <!--象征性按钮-->
     </v-bottom-sheet>
-    
-    <v-dialog v-model="openDialog">
+    <!-- go back dialog -->
+    <v-dialog v-model="openBackDialog">
       <v-card prepend-icon="mdi-update" max-width="400" class=""
         text="Analysis of cat's breed is in progress.
         Your image will still be saved but may not get result."
-        title="Are you sure to leave?">
+        title="Are you sure to go back?">
         
         <template v-slot:actions>
           <v-btn @click="goBack" variant="text">
-            Leave
+            Go Back
           </v-btn>
 
-          <v-btn @click="openDialog = false" class="flex-grow-1" color="primary" variant="tonal">
-            Cancel
+          <v-btn @click="openBackDialog = false" class="flex-grow-1" color="primary" variant="tonal">
+            Stay
           </v-btn>
         </template>
     </v-card>
     </v-dialog>
+    <!-- retry dialog -->
+    <v-dialog v-model="openRetryDialog">
+      <v-card prepend-icon="mdi-alert-circle-outline" max-width="400" class=""
+        text="Gemini API is overloaded. Please try again."
+        title="Analysis Failed">
+        
+        <template v-slot:actions>
+          <v-btn @click="openRetryDialog=false" variant="text">
+            Cancel
+          </v-btn>
 
+          <v-btn @click="retry" class="flex-grow-1" color="primary" variant="tonal">
+            Retry
+          </v-btn>
+        </template>
+    </v-card>
+    </v-dialog>
   </v-main>
   <VueEasyLightbox
             :visible="visible"
@@ -88,7 +104,8 @@ import VueEasyLightbox from 'vue-easy-lightbox'
 
 const router = useRouter()
 const openEdit = ref<Boolean>(false)
-const openDialog = ref<Boolean>(false)
+const openBackDialog = ref<Boolean>(false)
+const openRetryDialog = ref<Boolean>(false)
 const currCat = ref<Cat>({
   breed: 'Analysising...'
 })
@@ -97,28 +114,48 @@ const showChip = ref<Boolean>(true)
 const openSnackBar = ref<Boolean>(false)
 const visible = ref<Boolean>(false)  //whole picture
 const cameraInput = ref<HTMLInputElement | null>(null)
+let currentController: AbortController | null = null 
+let pollingTimer = null
 
 onMounted (() => {
+    putCat()
+})
+
+const putCat = () => {
+  if (currentController) {
+    currentController.abort()
+  }
+  currentController =  new AbortController()
+  if (pollingTimer) {
+    clearInterval(pollingTimer)
+    pollingTimer = null
+  }
   const catImg = tempUploadStore.file
   if(catImg) {
     const formData = new FormData()
     formData.append('image',catImg)
+    isLoading.value = true
     axios.put('http://localhost:8000/cat', formData,{
+      signal: currentController.signal
     }).then(function(res){
         console.log(res.data)
         const taskid = res.data.analysis_task_id
         if(taskid){
           polling(taskid)
         }
-    }).catch(error => { console.log(error) })
+    }).catch(error => { 
+        if(axios.isCancel(error)){
+          console.log('privior request cancel')
+        }else {
+          console.log(error)
+        }
+    })
   }
-})
-
-
+}
 
 const polling = (id:number) => {
-  isLoading.value = true
-  const timer = setInterval(() => {
+  
+  pollingTimer = setInterval(() => {
     axios.get(`http://localhost:8000/cat-breed/task/${id}`)
     .then(function(res){
       const status = res.data.status
@@ -127,11 +164,16 @@ const polling = (id:number) => {
         console.log(currCat.value.breed)
         isLoading.value = false
         openSnackBar.value = true
-        clearInterval(timer)
+        clearInterval(pollingTimer)
+        pollingTimer = null
       } else if(status === 'FAILED'){
         isLoading.value = false
-        clearInterval(timer)
-        alert('Analysis Failed. Please try again.')
+        clearInterval(pollingTimer)
+        // alert('Gemini API is overloaded. Please try again later.')
+        currCat.value.breed = 'Analysis Failed'
+        //应该打开dialog给用户retry的选项
+        openRetryDialog.value = true
+        pollingTimer = null
       }
     })
   }, 1000)
@@ -139,19 +181,19 @@ const polling = (id:number) => {
 
 const checkStatus = () => {
   if (isLoading.value === true){
-    openDialog.value = true
-    console.log(openDialog.value)
+    openBackDialog.value = true
+    console.log(openBackDialog.value)
   } else {
     console.log(isLoading.value)
-    console.log(openDialog.value)
+    console.log(openBackDialog.value)
     goBack()
   }
 }
 
 const goBack = () => {
-clearTempFile()
-console.log("已清除图片链接")
-router.back()
+    clearTempFile()
+    console.log("已清除图片链接")
+    router.back()
 }
 
 //NOTE：不管按钮点save还是cancel都会执行save函数
@@ -167,17 +209,27 @@ const save = (currCat: Cat, index) => {
 }
 
 const hideChip = () => {
-  showChip.value = false
+    showChip.value = false
+}
+const retry = () => {
+    putCat()
+    openRetryDialog.value = false
 }
 
 const onFileSelected = (e: any) => {
     const selectedFile = e.target.files[0]
     if (selectedFile) {
         setTempFile(selectedFile)
-        
+        putCat()
         e.target.value = ''
     }
+    
 }
+
+onUnmounted(() => {
+    if (currentController) currentController.abort()
+    // if (pollingTimer) clearInterval(pollingTimer)
+})
 
 </script>
 <style scoped>
