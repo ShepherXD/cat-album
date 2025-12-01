515fd6ca473bf49f4fc33d1100fc5d537562df02
ShepherXD <yingzhengdev@gmail.com>
2025-12-02 03:39:02 +0900

    add google material design icon support

--- frontend/src/plugins/vuetify.ts
+++ frontend/src/plugins/vuetify.ts
@@ -7,12 +7,21 @@
 // Styles
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
+import 'material-design-icons-iconfont/dist/material-design-icons.css' // Ensure your project is capable of handling css files
+import { aliases, md } from 'vuetify/iconsets/md'
 
 // Composables
 import { createVuetify } from 'vuetify'
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      md,
+    },
+  },
   theme: {
     defaultTheme: 'light',
   },
